6859e20896d0d6db257eae2df9c71cae97530da6
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2021-04-17 23:39:44 +0300

    17.04

--- src/streamOrnekler/Giticindeneme.java
+++ src/streamOrnekler/Giticindeneme.java
@@ -3,5 +3,9 @@ package streamOrnekler;
 public class Giticindeneme {
     public static void main(String[] args) {
         System.out.println("asdadadadadadad");
+        System.out.println("Ali Baba");
+        System.out.println("rasgele harf");
+
+        System.out.println("rasgele harf");
     }
 }
